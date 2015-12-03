a9d0ecece609d79a98ef47370774aa94e5acaeaf
sachinnair92 <sachinnair92@gmail.com>
2015-12-04 00:22:07 +0530

    added filter

---
+++ src/main/java/com/grocberry/Filter/ResponseCorsFilter.java
@@ -0,0 +1,33 @@
+package com.grocberry.Filter;
+
+
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+
+/**
+ * Created by Voodoo on 03/12/15.
+ */
+@Provider
+public class ResponseCorsFilter implements ContainerResponseFilter {
+
+
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Headers", "");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        containerResponseContext.getHeaders().add("Access-Control-Max-Age", "1209600");
+
+       // cres.getHttpHeaders().add("Access-Control-Allow-Origin", "*");
+        //cres.getHttpHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+       // cres.getHttpHeaders().add("Access-Control-Allow-Credentials", "true");
+       // cres.getHttpHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        //cres.getHttpHeaders().add("Access-Control-Max-Age", "1209600");
+    }
+}
